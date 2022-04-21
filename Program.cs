c1339ca1809566ee750a4e6b6e4e67f2a1093429
Isaac <isaaccreer@gmail.com>
2022-04-21 15:09:08 -0600

    Added the Hello world

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hello_world
+{
+    public class Hello_world
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
